bc48cebea4958fc66274b4b743a1a1c4bf1b0ea7
ajt9 <atoma840@live.kutztown.edu>
2026-02-27 01:30:33 -0500

    Create calculator webapp

---
+++ Client/Weapon.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp.Client
+{
+    public class Weapon
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public string imgUrl { get; set; }
+
+        public Weapon(string name, int price, string imgUrl)
+        {
+            this.name = name;
+            this.price = price;
+            this.imgUrl = imgUrl;
+        }
+
+    }
+}
